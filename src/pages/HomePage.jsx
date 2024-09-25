39204325b8f0dcfbe85db4cc202ae0196cdd8830
pywon128 <58430024+pywon128@users.noreply.github.com>
2024-09-25 19:37:31 +0900

    [Feat] 회원가입 페이지 및 기능 구현 (#19)
    * [Add]모임 상세 추가
    
    * [Add]router-dom 설치
    
    * [Feat] 로그인 모달창 구현
    
    * [Feat] 회원가입 페이지 및 기능 구현
    
    ---------
    
    Co-authored-by: ghdtjrdud38@gmail.com <ghdtjrdud38@gmail.com>

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,16 @@
+import Header from '../components/header/Header';
+import '../index.css';
+
+const MainPage = () => {
+  
+
+  return (
+    <div>
+      <Header />
+      <h1>홈페이지</h1>
+      
+    </div>
+  );
+};
+
+export default MainPage;
