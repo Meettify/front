520fe1f0d6ac0aaacd774372061d7227ad12d911
yuna2002 <dladbsk674@gmail.com>
2024-09-30 14:26:12 +0900

    [Feat] 채팅페이지

--- src/pages/layout/BasicLayout.jsx
+++ src/pages/layout/BasicLayout.jsx
@@ -1,7 +1,5 @@
 import Header from "../../components/header/Header";
-import Recomend from "../../components/meeting/Recomend";
 import Footer from "../../components/footer/Footer";
-import { Route } from "react-router-dom";
 import AppRouter from "../../router/AppRouter";
 
 const BasicLayout = () => {
