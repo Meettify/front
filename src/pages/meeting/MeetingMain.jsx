a52b82e91e801f97785dd503e2c4f6b669956f5e
yuna2002 <dladbsk674@gmail.com>
2024-09-30 14:33:28 +0900

    [Feat] 채팅 페이지 (#29)
    * [Add]모임 상세 추가
    
    * [Add]router-dom 설치
    
    * [Feat] 로그인 모달창 구현
    
    * [Feat] 회원가입 페이지 및 기능 구현
    
    * [Fix] 헤더 컴포넌트 사용
    
    * [Feat] 모임 메인페이지 컴포넌트 구분
    
    * [Feat] 추천상품 생성
    
    * [Feat] 소모임 메인페이지 구성
    
    * [Fix] 오류 수정
    
    * [Add]모임 수정페이지
    
    * [Add] Meeting update 생성
    
    * [Fix] tailwind 설정 통합
    
    * [Feat] 채팅 컴포넌트
    
    * [Fix] 머지충돌해결
    
    * [Fix] Router, layout 수정
    
    * [Feat] 채팅페이지
    
    ---------
    
    Co-authored-by: ghdtjrdud38@gmail.com <ghdtjrdud38@gmail.com>
    Co-authored-by: Jaewon <dlwodnjs0128@gmail.com>
    Co-authored-by: pywon128 <58430024+pywon128@users.noreply.github.com>

--- src/pages/meeting/MeetingMain.jsx
+++ src/pages/meeting/MeetingMain.jsx
@@ -1,7 +1,6 @@
-import Header from "../../components/header/Header";
 import Category from "../../components/meeting/Category";
 import Recomend from "../../components/meeting/Recomend";
-import Footer from "../../components/footer/Footer";
+
 const MeetingMain = () => {
     return (
         <div className="flex justify-between px-4"> {/* Flexbox로 가로로 배치 */}
