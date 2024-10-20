6d9fc447ca0be5989ff1b8053af3331315be2230
sxu <bnsxu@naver.com>
2024-10-20 19:56:44 +0900

    [Modify] 메인, 상품 페이지 수정

--- src/pages/admin/AdminPage.jsx
+++ src/pages/admin/AdminPage.jsx
@@ -10,7 +10,7 @@ const AdminPage = () => {
     }
 
     return (
-        <div className="flex justify-center items-start min-h-screen p-10 bg-gray-50">
+        <div className="flex justify-center items-start min-h-screen p-10">
             {/* 왼쪽 메뉴 영역 */}
             <aside className="w-1/5 p-6 border-r border-gray-300">
                 <h1 className="text-3xl font-bold mb-8 text-left">관리자 설정</h1>
@@ -26,14 +26,16 @@ const AdminPage = () => {
                     <h2 className="font-semibold mb-3 text-left text-xl">상품 관리</h2>
                     <ul className="space-y-2 text-left">
                         <li><Link to="itemList">상품 조회</Link></li>
-                        <li><Link to="itemAdd">상품 등록</Link></li>
+                        <li><Link to="itemAdd">상품 등록 내역</Link></li>
+                        <li><Link to="itemAdd">현재 재고 내역</Link></li>
                     </ul>
                 </section>
 
                 <section>
                     <h2 className="font-semibold mb-3 text-left text-xl">고객센터 관리</h2>
                     <ul className="space-y-2 text-left">
-                        <li><Link to="inquiry-list">공지사항 조회</Link></li>
+                        <li><Link to="inquiry-list">공지사항 등록</Link></li>
+                        <li><Link to="inquiry-list">전체 문의 내역</Link></li>
                     </ul>
                 </section>
             </aside>
