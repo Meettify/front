b696655855108a1fda29c16954285a6ef0968255
DESKTOP-6AL5I4J\Won <dlwodnjs0128@gmail.com>
2024-11-23 15:35:09 +0900

    [Modify] 마이페이지 수정

--- src/stores/useMypageStore.js
+++ src/stores/useMypageStore.js
@@ -3,19 +3,29 @@ import { create } from 'zustand';
 const useMypageStore = create((set) => ({
     meetJoinList : [],
 
-    myPostList : [],
+    posts : [],
     totalPages: 1,
     currentPage: 1,
     totalPostCount: 0,
     
-    setMeetJoinList: (meets) => {
-        set({ meetJoinList: meets });
-    },
+    inquirys: [],
+    inquiryTotalPages: 1,
+    inquiryCurrentPage: 1,
+    totalInquiryCount: 0,
+    totalInquiryOkCount: 0,
+
+    setMeetJoinList: (meets) => {set({ meetJoinList: meets });},
 
     setPosts: (posts) => set({ posts }),
     setTotalPages: (totalPages) => set({ totalPages }),
     setCurrentPage: (currentPage) => set({ currentPage }),
     setTotalPostCount: (totalPostCount) => set({totalPostCount}),
+
+    setInquirys: (inquirys) => set({inquirys}),
+    setInquiryTotalPages: (inquiryTotalPages) => set({inquiryTotalPages}),
+    setInquiryCurrentPage: (inquiryCurrentPage) => set({inquiryCurrentPage}),
+    setTotalInquiryCount: (totalInquiryCount) => set({totalInquiryCount}),
+    setTotalInquiryOkCount: (totalInquiryOkCount) => set({totalInquiryOkCount}),
 
 }));
 
