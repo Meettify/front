38b7cf461ad8f29589044762f22d756b34e511a8
sxu <bnsxu@naver.com>
2024-12-08 23:55:58 +0900

    [Feat] 결제 위젯 추가

--- src/pages/order/OrderPage.jsx
+++ src/pages/order/OrderPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { useAuth } from '../../hooks/useAuth';
-import CheckoutPage from '../../components/payment/CheckoutPage';
+import { CheckoutPage } from '../../components/payment/CheckoutPage';
 
 const OrderPage = () => {
     const location = useLocation();
