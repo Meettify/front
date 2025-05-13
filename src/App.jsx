79ce2324e8e359d70b4dc60196e071ee0799e417
YuYoHan <zxzz8014@naver.com>
2025-05-13 23:08:42 +0900

    [Refactor] App 리팩토링

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import './App.css'
-import React from 'react';
-import { RouterProvider } from 'react-router-dom'
-import root from './routers/root';
-import useNotificationSSE from './hooks/useNotificationSSE';
+import "./App.css";
+import { React } from "react";
+import { RouterProvider } from "react-router-dom";
+import root from "./routers/root";
+import useNotificationSSE from "./hooks/useNotificationSSE";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
